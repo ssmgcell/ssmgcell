9c457c45db6de38ae1a2448edd3600f02b3bf558
ssmgcell <39262571+ssmgcell@users.noreply.github.com>
2018-05-14 15:31:42 +0530

    hj

---
+++ admin/include/footer.php
@@ -0,0 +1,9 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<div class="text-center">
+                                  <p>Copyright &copy; <a href="http:/www.ssmengg.edu.in/">SSM_MCA</a> | SSM Grievance Management System 2018</p>
+              <a href="#" class="go-top">
+		</div>
+	</div>
